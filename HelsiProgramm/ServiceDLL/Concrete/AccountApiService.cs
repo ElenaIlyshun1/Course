ac5ea9b44799105986f639c0406ff815678647f8
Роман Максютинський <maksutroman@outlook.com>
2019-10-31 12:35:45 +0200

    Add Api

--- HelsiProgramm/ServiceDLL/Concrete/AccountApiService.cs
+++ HelsiProgramm/ServiceDLL/Concrete/AccountApiService.cs
@@ -14,28 +14,28 @@ namespace ServiceDLL.Concrete
     public class AccountApiService
     {
         private string _url = "https://localhost:44340/api/user";
-        public int Login(LoginViewModel user)
-        {
-            var http = (HttpWebRequest)WebRequest.Create(new Uri(_url));
-            http.Accept = "application/json";
-            http.ContentType = "application/json";
-            http.Method = "POST";
+        //public int Login(LoginViewModel user)
+        //{
+        //    var http = (HttpWebRequest)WebRequest.Create(new Uri(_url));
+        //    http.Accept = "application/json";
+        //    http.ContentType = "application/json";
+        //    http.Method = "POST";
 
-        //    string parsedContent = JsonConvert.SerializeObject(user);
-        //    UTF8Encoding encoding = new UTF8Encoding();
-        //    Byte[] bytes = encoding.GetBytes(parsedContent);
+        ////    string parsedContent = JsonConvert.SerializeObject(user);
+        ////    UTF8Encoding encoding = new UTF8Encoding();
+        ////    Byte[] bytes = encoding.GetBytes(parsedContent);
 
-        //    Stream newStream = http.GetRequestStream();
-        //    newStream.Write(bytes, 0, bytes.Length);
-        //    newStream.Close();
+        ////    Stream newStream = http.GetRequestStream();
+        ////    newStream.Write(bytes, 0, bytes.Length);
+        ////    newStream.Close();
 
 
-            var response = http.GetResponse();
-            var stream = response.GetResponseStream();
-            var sr = new StreamReader(stream);
-            var content = sr.ReadToEnd();
-            return 0;
-        }
+        //    var response = http.GetResponse();
+        //    var stream = response.GetResponseStream();
+        //    var sr = new StreamReader(stream);
+        //    var content = sr.ReadToEnd();
+        //    return 0;
+        //}
 
 
         public int Register(ClientAddVM user)
