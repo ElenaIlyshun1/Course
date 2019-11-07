4b70e59b8e40f140977d9dab7a71a2e913d2c8b0
lenailyshun <lenailyshun@gmail.com>
2019-11-07 11:47:41 +0200

    Add registration

--- HelsiProgramm/HelsiProgramm/LoginForm.cs
+++ HelsiProgramm/HelsiProgramm/LoginForm.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using ServiceDLL.Concrete;
+using ServiceDLL.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -31,14 +37,81 @@ namespace HelsiProgramm
 
         private void btnSignIn_Click(object sender, EventArgs e)
         {
-            MainForm mainForm = new MainForm();
-            mainForm.ShowDialog();
+            lblErrorEmail.Text = "";
+            lblErrorPassword.Text = "";
+            if (txtEmail.Text == " " && txtPswd.Text == " ")
+            {
+                MessageBox.Show("Please, fill all lines.");
+            }
+            else
+            {
+                // відправляємо модель на сервер
+                AccountApiService service = new AccountApiService();
+                try
+                {
+                    var token = service.Login(new UserLoginVM
+                    {
+                        Password = txtPswd.Text,
+                        Email = txtEmail.Text
+                    });
+
+                    var tokenStr = token;
+                    var handler = new JwtSecurityTokenHandler();
+                    var tokenJwtSec = handler.ReadToken(tokenStr) as JwtSecurityToken;
+
+                    //Dictionary<string, string> user = new Dictionary<string, string>();
+
+                    //foreach (var item in tokenJwtSec.Claims)
+                    //{
+                    //    //if (item.Type != "Id" && item.Type != "exp")
+                    //    //MessageBox.Show(item.Type, item.Value);
+                    //    //user.Add(item.Type, item.Value);
+                    //}
+                    //// WriteSomeTextAsync(txtEmail.Text + " " + token);
+                    ///
+                    
+                    MainForm mainForm = new MainForm();
+                    mainForm.ShowDialog();
+                    this.Close();
+                }
+                catch (WebException wex)
+                {
+                    if (wex.Response != null)
+                    {
+                        using (var errorResponse = (HttpWebResponse)wex.Response)
+                        {
+                            using (var reader = new StreamReader(errorResponse.GetResponseStream()))
+                            {
+                                var error = reader.ReadToEnd();
+                                var mes = JsonConvert.DeserializeAnonymousType(error, new
+                                {
+                                    Email = "",
+                                    Password = ""
+                                });
+                                if (mes.Email != null)
+                                {
+                                    lblErrorEmail.Text = mes.Email;
+                                }
+                                if (mes.Password != null)
+                                {
+                                    lblErrorPassword.Text = mes.Password;
+                                }
+                                lblErrorEmail.ForeColor = Color.Red;
+                                lblErrorPassword.ForeColor = Color.Red;
+                            }
+                        }
+                    }
+                }
+
+            }
+            
         }
 
         private void txtPswd_OnValueChanged(object sender, EventArgs e)
         {
             txtPswd.isPassword = true;
         }
-        #endregion
+
     }
+    #endregion
 }
