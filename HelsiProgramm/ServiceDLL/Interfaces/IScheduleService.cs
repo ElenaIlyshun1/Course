099e091a8bcd1c49d5fd8b896ed0e3e0060971fd
maksutroman <maksutroman@A24PC7>
2019-11-13 13:47:04 +0200

    Add conect to Schedule

---
+++ HelsiProgramm/ServiceDLL/Interfaces/IScheduleService.cs
@@ -0,0 +1,16 @@
+﻿using ServiceDLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDLL.Interfaces
+{
+    interface IScheduleService
+    {
+        List<ScheduleModel> GetSchedule();
+        Task<List<ScheduleModel>> GetScheduleAsync();
+        string CreateSchedule(ScheduleAddModel schedule);
+    }
+}
